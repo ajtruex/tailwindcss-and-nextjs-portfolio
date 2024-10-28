3b23e57420b8df3927c2291a250d0f954fa1a88d
Andrew Truex <andrewtruex@gmail.com>
2024-10-28 04:33:32 -0400

    add env variables

--- app/api/analytics/route.js
+++ app/api/analytics/route.js
@@ -1,58 +1,10 @@
-// const url =
-//   "https://api.umami.is/v1/websites/c1ecb30b-bd10-49c9-8eae-34471d9e3830/stats?startAt=1688190600000&endAt=1730094590464"
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "x-umami-api-key": "Y7qVJSCzTLBanPALYTb3r0OLxLLZQ411",
-//     Accept: "application/json",
-//   },
-// }
-// export async function GET() {
-//   let data = await fetch(
-//     "https://api.umami.is/v1/websites/c1ecb30b-bd10-49c9-8eae-34471d9e3830/stats?startAt=1688190600000&endAt=1730094590464"
-//   )
-//   try {
-//     const response = await fetch(url, options)
-//     const data = await response.json()
-//     console.log(data)
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
-// const url =
-//   "https://api.umami.is/v1/websites/c1ecb30b-bd10-49c9-8eae-34471d9e3830/stats?startAt=1688190600000&endAt=1730094590464"
-// const options = {
-//   method: "GET",
-//   headers: {
-//     Accept: "application/json",
-//     "x-umami-api-key": "Y7qVJSCzTLBanPALYTb3r0OLxLLZQ411",
-//   },
-// }
-
-// export async function GET() {
-//   try {
-//     const response = await fetch(url, options)
-//     const data = await response.json()
-//     console.log(data)
-//   } catch (error) {
-//     console.error(error)
-//   }
-
-//   return new Response(JSON.stringify(data), {
-//     status: 200,
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Cache-Control": "max-age=60, s-maxage=60, stale-while-revalidate=600",
-//     },
-//   })
-// }
 const url =
   "https://api.umami.is/v1/websites/c1ecb30b-bd10-49c9-8eae-34471d9e3830/stats?startAt=1688190600000&endAt=1730094590464"
 const options = {
   method: "GET",
   headers: {
     Accept: "application/json",
-    "x-umami-api-key": "Y7qVJSCzTLBanPALYTb3r0OLxLLZQ411",
+    "x-umami-api-key": `${process.env.UMAMI_API_KEY}`,
   },
 }
 
--- app/api/playcount/route.js
+++ app/api/playcount/route.js
@@ -1,19 +1,6 @@
 import { getPlaycount } from "@/lib/spotify"
 
-// export default async function handler(req) {
-//   const resp = await getPlaycount()
-//   console.log(resp)
-//   return new Response(JSON.stringify(resp), {
-//     status: 200,
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Cache-Control": "max-age=60, s-maxage=60, stale-while-revalidate=600",
-//     },
-//   })
-// }
-
-const url =
-  "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=ajtruex&api_key=22b72a9335e28b31e18527b6122ce723&format=json"
+const url = `http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=ajtruex&api_key=${process.env.LASTFM_API_KEY}&format=json`
 const options = { method: "GET" }
 export async function GET(request) {
   try {
