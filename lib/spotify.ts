3045ff70af540abb6914eb9c30c514c8faf6b74f
Andrew Truex <andrewtruex@gmail.com>
2024-12-03 15:07:51 -0500

    update fetch requests in Spotify API to disable caching and enforce dynamic responses

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -15,8 +15,10 @@ export async function getTopArtists() {
   return fetch(
     `http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=ajtruex&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=10&period=1month`,
     {
+      cache: "no-store",
       headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
       },
     }
   )
@@ -25,8 +27,10 @@ export async function getTopArtistsYear() {
   return fetch(
     `http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=ajtruex&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=10&period=12month`,
     {
+      cache: "no-store",
       headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
       },
     }
   )
@@ -35,8 +39,10 @@ export async function getTopTracks() {
   return fetch(
     `http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=ajtruex&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=10&period=1month`,
     {
+      cache: "no-store",
       headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
       },
     }
   )
@@ -45,8 +51,10 @@ export async function getTopTracksYear() {
   return fetch(
     `http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=ajtruex&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=10&period=12month`,
     {
+      cache: "no-store",
       headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
       },
     }
   )
@@ -56,8 +64,10 @@ export async function getPlaycount() {
   return fetch(
     `http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=ajtruex&api_key=${process.env.LASTFM_API_KEY}&format=json`,
     {
+      cache: "no-store",
       headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
       },
     }
   )
