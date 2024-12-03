196fd89d4243afe5af826e3235b855a5561f4b67
Andrew Truex <andrewtruex@gmail.com>
2024-12-03 15:39:54 -0500

    Create route.ts

---
+++ app/api/spotify/now-playing/route.ts
@@ -0,0 +1,33 @@
+import { getNpOrRpSong } from "@/lib/spotify"
+
+export const dynamic = "force-dynamic"
+
+export async function GET(request: Request) {
+  const resp = await getNpOrRpSong()
+
+  if (resp.status !== 200) {
+    return new Response(JSON.stringify(await resp.json()), {
+      status: resp.status,
+    })
+  }
+
+  const response = await resp.json()
+
+  const track = response.recenttracks.track[0]
+
+  const nowPlaying = {
+    isPlaying: track["@attr"]?.nowplaying || false,
+    name: track.name,
+    artist: track.artist["#text"],
+    url: track.url,
+    image: track.image[3]["#text"],
+  }
+
+  return new Response(JSON.stringify(nowPlaying), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store, max-age=0",
+    },
+  })
+}
